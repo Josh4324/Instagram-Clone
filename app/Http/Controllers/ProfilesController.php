94f6738c51b4f2b9aacca551dc17a343c4227d19
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-07-02 18:45:56 +0100

    initial commit

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('home');
+    }
+}
